9a4dc65509481f12925c661b266aa7ddc4bdb5fc
IAmVolvic <71038869+IAmVolvic@users.noreply.github.com>
2023-11-29 12:59:47 +0100

    Update MediaController.java

--- src/DLL/Components/MediaController.java
+++ src/DLL/Components/MediaController.java
@@ -33,8 +33,8 @@ public class MediaController {
     public void setVolume(Double newVolume) {
         if(fileLoaded){
             mPlayer.setVolume(newVolume);
-            lastVolume = newVolume;
         }
+        lastVolume = newVolume;
     }
 
 }
