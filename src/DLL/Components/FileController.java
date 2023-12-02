8ec5e6b40673076f123f18f7c9895cab9c9a4d23
IAmVolvic <71038869+IAmVolvic@users.noreply.github.com>
2023-12-02 14:54:37 +0100

    Cleaned Up code

--- src/DLL/Components/FileController.java
+++ src/DLL/Components/FileController.java
@@ -1,9 +1,23 @@
 package DLL.Components;
 
+import BE.Playlist;
+import DAL.Logic.MyPlaylistController;
+
 import java.io.File;
+import java.util.ArrayList;
+
 public class FileController {
+    private MyPlaylistController myPlaylist;
+    private String playlistPath = "resources/Playlists/";
+
     File musicFolder = new File("resources/music");
     File[] listOfFiles = musicFolder.listFiles();
+
+    public FileController(MyPlaylistController mPlayList) {
+        myPlaylist = mPlayList;
+        createPlaylistPath();
+        playlistPathCleanup();
+    }
 
 
     private boolean isValid() {
@@ -13,6 +27,7 @@ public class FileController {
         }
         return true;
     }
+
 
     public File getSong(String songName) {
         isValid();
@@ -27,4 +42,58 @@ public class FileController {
         return null;
     }
 
+
+    private void createPlaylistPath(){
+        int numberOfPlaylists = myPlaylist.getAllPlaylists().size();
+
+        for(Playlist playlist : myPlaylist.getAllPlaylists()){
+            if (numberOfPlaylists > 0){
+                createPath(playlist.PlaylistName());
+            }
+        }
+    }
+
+
+    private void playlistPathCleanup() {
+        File[] allContents = new File(playlistPath).listFiles();
+
+        if (allContents != null) {
+            for (File file : allContents) {
+                if (checkIfPlaylistExists(file.getName())) {
+                    deletePath(file);
+                }
+            }
+        }
+    }
+
+
+    private boolean checkIfPlaylistExists(String check) {
+        for (Playlist playlist : myPlaylist.getAllPlaylists()) {
+            if (playlist.PlaylistName().equals(check)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    private void createPath(String fileName){
+        File theDir = new File(playlistPath + fileName);
+
+        if (theDir.mkdirs()) {
+            System.out.println("Folder created successfully.");
+        }
+    }
+
+
+    private boolean deletePath(File path) {
+        File[] allContents = path.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deletePath(file);
+            }
+        }
+        return path.delete();
+    }
 }
--- src/DLL/DllController.java
+++ src/DLL/DllController.java
@@ -4,25 +4,27 @@ import BE.Playlist;
 import DAL.Logic.MyPlaylistController;
 import DLL.Components.MediaController;
 import DLL.Components.FileController;
-import java.io.File;
 import java.util.ArrayList;
 
 public class DllController {
-    private final String currentSong = "ThxSoMchHate1.mp3";
+    private final String currentSong = "ThxSoMchHate.mp3";
 
     // Controllers
-    FileController fileController = new FileController();
-    MediaController mediaController = new MediaController();
     MyPlaylistController myPlaylist = new MyPlaylistController();
-    public DllController(){
-        createPlaylistPath();
-    }
+    FileController fileController = new FileController(myPlaylist);
+    MediaController mediaController = new MediaController();
+
+
+
+    public DllController(){}
+
 
     public void PlaySong(){
         if(fileController.getSong(currentSong) != null){
             mediaController.playSong(fileController.getSong(currentSong));
         }
     }
+
 
     public void PauseSong(){
         mediaController.pauseSong();
@@ -33,25 +35,8 @@ public class DllController {
         mediaController.setVolume(newVolume);
     }
 
+
     public ArrayList<Playlist> getPlaylists() {
         return myPlaylist.getAllPlaylists();
     }
-
-
-    private void createPlaylistPath(){
-        int numberOfPlaylists = myPlaylist.getAllPlaylists().size();
-        String playlistPath = "resources/Playlists/";
-
-        for(Playlist playlist : myPlaylist.getAllPlaylists()){
-            File theDir = new File(playlistPath + playlist.PlaylistName());
-
-            System.out.println(numberOfPlaylists);
-            if (numberOfPlaylists > 0){
-                if (theDir.mkdirs()) {
-                    System.out.println("Folder created successfully.");
-                }
-            }
-        }
-    }
-
 }
